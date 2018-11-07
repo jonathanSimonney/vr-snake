911e4eb642bd0836f000f35dde9eddc1c31cd69b
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-11-07 15:24:51 +0100

    movement operational.

--- assets/components.js
+++ assets/components.js
@@ -1,2 +1,39 @@
+let initialPlayerRotation;
+
+AFRAME.registerComponent('always-moving', {
+    schema: {
+        speed: {type: 'float', default: 0.05}, // Décalage sur l'axe (step)
+    },
+
+    init: function(){
+        initialPlayerRotation = this.el.getAttribute("rotation");
+    },
 
 
+
+    tick: function () {
+        const cameraRotation = document.querySelector('#camera').getAttribute("rotation")
+
+        console.log(cameraRotation)
+        rxDecalage = cameraRotation.x
+        ryDecalage = cameraRotation.y
+        //rzDecalage = cameraRotation.z
+
+        const rx = initialPlayerRotation.x + rxDecalage
+        const ry = initialPlayerRotation.y + ryDecalage
+        //const rz = initialPlayerRotation.z + rzDecalage
+
+        console.log(rx, ry);
+        var currentPos = this.el.getAttribute('position'); //Position actuel de l'objet a déplacer
+
+        const newPosition = {
+            x: currentPos.x + this.data.speed * Math.sin((ry - 180) * Math.PI / 180.0), //Nouvelle valeur pour la position x
+            y: currentPos.y + this.data.speed * Math.tan((rx - 180) * (Math.PI / 180)), //Nouvelle valeur pour la postion y
+            z: currentPos.z + this.data.speed * Math.cos((ry - 180) * Math.PI / 180.0), //Nouvelle valeur pour la postion z
+        };
+
+        console.log(currentPos, newPosition)
+
+        this.el.setAttribute('position', newPosition);
+    }
+});
