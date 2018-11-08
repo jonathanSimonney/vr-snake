f4a1ca42136827c7aacbdc7833087aec3bc181c0
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-11-08 09:17:57 +0100

    movement kinda functional but not in 3D.

--- assets/components.js
+++ assets/components.js
@@ -14,15 +14,15 @@ AFRAME.registerComponent('always-moving', {
     tick: function () {
         const cameraRotation = document.querySelector('#camera').getAttribute("rotation")
 
-        rxDecalage = cameraRotation.x
-        ryDecalage = cameraRotation.y
-        //rzDecalage = cameraRotation.z
+        const rxDecalage = cameraRotation.x
+        const ryDecalage = cameraRotation.y
+        // rzDecalage = cameraRotation.z
 
         const rx = initialPlayerRotation.x + rxDecalage
         const ry = initialPlayerRotation.y + ryDecalage
         //const rz = initialPlayerRotation.z + rzDecalage
 
-        var currentPos = this.el.getAttribute('position'); //Position actuel de l'objet a déplacer
+        var currentPos = this.el.getAttribute('position'); //Position actuelle de l'objet a déplacer
 
         const newPosition = {
             x: currentPos.x + this.data.speed * Math.sin((ry - 180) * Math.PI / 180.0), //Nouvelle valeur pour la position x
@@ -32,24 +32,6 @@ AFRAME.registerComponent('always-moving', {
 
         // console.log(currentPos, newPosition)
 
-        // this.el.setAttribute('position', newPosition);
-    }
-});
-
-AFRAME.registerComponent('missile', {
-    schema: {
-        speed: {type: 'float', default: 0.1}, // Décalage sur l'axe (step)
-        rx: {type: 'float'}, //Angle x de l'axe à suivre (rotation.x)
-        ry: {type: 'float'}, //Angle y de l'axe à suivre (rotation.y)
-        rz: {type: 'float'}, //Angle z de l'axe à suivre (rotation.z)
-    },
-
-    tick: function () {
-        var currentPos = this.el.getAttribute('position'); //Position actuel de l'objet a déplacer
-        this.el.setAttribute('position', {
-            x: currentPos.x + this.data.speed * Math.sin((this.data.ry - 180) * Math.PI / 180.0), //Nouvelle valeur pour la position x
-            y: currentPos.y + this.data.speed * Math.tan((this.data.rx - 180) * (Math.PI / 180)), //Nouvelle valeur pour la postion y
-            z: currentPos.z + this.data.speed * Math.cos((this.data.ry -180) * Math.PI / 180.0), //Nouvelle valeur pour la postion z
-        });
+        this.el.setAttribute('position', newPosition);
     }
 });
